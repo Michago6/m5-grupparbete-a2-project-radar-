0211124f12735244e1034cb7e9b2765a8070a712
Michago6 <144434488+Michago6@users.noreply.github.com>
2023-09-08 10:57:33 +0200

    Initiala filer för MakeCode-projekt

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Öppna denna sida på [https://michago6.github.io/m5-grupparbete-a2-project-radar-/](https://michago6.github.io/m5-grupparbete-a2-project-radar-/)
+
+## Använd som tillägg
+
+Denna databas kan läggas till som ett **tillägg** i MakeCode.
+
+* öppna [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* klicka på **Nytt projekt**
+* klicka på **Tillägg** under kugghjulsmenyn
+* sök efter **https://github.com/michago6/m5-grupparbete-a2-project-radar-** och importera
+
+## Redigera detta projekt
+
+Redigera den här databasen i MakeCode.
+
+* öppna [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* klicka på **Importera** och klicka sedan på **Importera URL**
+* klistra in **https://github.com/michago6/m5-grupparbete-a2-project-radar-** och klicka på importera
+
+#### Metadata (används för sökning och rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,85 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (received == 0) {
+        if (grupp == 114) {
+            signalStrength1 = radio.receivedPacket(RadioPacketProperty.SignalStrength)
+        } else {
+            if (grupp == 115) {
+                signalStrength2 = radio.receivedPacket(RadioPacketProperty.SignalStrength)
+                xMax = receivedNumber
+            }
+        }
+        received = 1
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    if (visa == 1) {
+        calibrate += 5
+        basic.showNumber(calibrate)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    if (visa == 0) {
+        visa = 1
+    } else {
+        visa = 0
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (visa == 1) {
+        calibrate += -5
+        basic.showNumber(calibrate)
+    }
+})
+let xFinal = 0
+let xRadar = 0
+let yRadar = 0
+let x = 0
+let y = 0
+let signalStrength2 = 0
+let signalStrength1 = 0
+let calibrate = 0
+let xMax = 0
+let visa = 0
+let received = 0
+let grupp = 0
+radio.setGroup(114)
+grupp = 114
+received = 0
+visa = 0
+xMax = 2
+calibrate = 45
+basic.forever(function () {
+    if (received == 1) {
+        grupp += 1
+        if (grupp == 116) {
+            grupp = 114
+        }
+        radio.setGroup(grupp)
+        received = 0
+        y = ((0 - signalStrength2 - calibrate) ** 2 + (0 - xMax - calibrate) ** 2 - (0 - signalStrength1 - calibrate) ** 2) / (2 * (0 - xMax - calibrate))
+        x = Math.sqrt((0 - signalStrength2 - calibrate) ** 2 - y ** 2)
+        yRadar = Math.round(5 * y / (0 - xMax - calibrate))
+        xRadar = Math.round(5 * x / (0 - xMax - calibrate))
+        if (xRadar > 4) {
+            xFinal = 4
+        } else {
+            if (xRadar < 0) {
+                xFinal = 0
+            } else {
+                xFinal = xRadar
+            }
+        }
+        if (visa == 0) {
+            basic.clearScreen()
+            if (yRadar > 4) {
+                led.plot(xFinal, 4)
+            } else {
+                if (yRadar < 0) {
+                    led.plot(xFinal, 0)
+                } else {
+                    led.plot(xFinal, yRadar)
+                }
+            }
+        }
+    }
+})
